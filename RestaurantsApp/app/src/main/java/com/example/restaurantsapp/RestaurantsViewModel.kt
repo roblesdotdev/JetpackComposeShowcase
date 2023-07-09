3bfdab52e4c670ae3d8288b588e78edaaf0e4377
roblesdotdev <httpsnip@gmail.com>
2023-07-09 18:11:46 -0300

    restaurants app: api request with retrofit

--- RestaurantsApp/app/src/main/java/com/example/restaurantsapp/RestaurantsViewModel.kt
+++ RestaurantsApp/app/src/main/java/com/example/restaurantsapp/RestaurantsViewModel.kt
@@ -4,10 +4,52 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.restaurantsapp.data.Restaurant
-import com.example.restaurantsapp.data.dummyRestaurants
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RestaurantsViewModel(private val stateHandle: SavedStateHandle): ViewModel() {
-    val state = mutableStateOf(dummyRestaurants.restoreSelections())
+    private var restInterface: RestaurantsApiService
+    val state = mutableStateOf(emptyList<Restaurant>())
+    private lateinit var restaurantsCall: Call<List<Restaurant>>
+
+    init  {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .baseUrl("https://jc-restaurants-default-rtdb.firebaseio.com/")
+            .build()
+        restInterface = retrofit.create(RestaurantsApiService::class.java)
+        getRestaurants()
+    }
+
+    private fun getRestaurants() {
+        restaurantsCall = restInterface.getRestaurants()
+        restaurantsCall.enqueue(
+            object : Callback<List<Restaurant>> {
+                override fun onResponse(
+                    call: Call<List<Restaurant>>,
+                    response: Response<List<Restaurant>>
+                ) {
+                    response.body()?.let {restaurants ->
+                        state.value = restaurants.restoreSelections()
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            }
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        restaurantsCall.cancel()
+    }
 
     fun toggleFavorite(id: Int) {
         val restaurants = state.value.toMutableList()
